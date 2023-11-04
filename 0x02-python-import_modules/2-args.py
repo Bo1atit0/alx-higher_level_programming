283c56ebc068a19c4bfe613c0713ae459644e412
bo1atit0 <shorungbebolatito@gmail.com>
2023-11-04 14:30:04 -0700

    command line args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+lent = len(sys.argv) - 1
+if lent == 0:
+    print("{} arguments.".format(lent))
+elif lent == 1:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(lent))
+    for i in range(1, lent):
+        print("{}: {}".format(i, sys.argv[i]))
