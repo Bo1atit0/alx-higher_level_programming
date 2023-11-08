ea3e0c8ac5aa8955e38fe8dfda678888140903ff
bo1atit0 <shorungbebolatito@gmail.com>
2023-11-08 14:20:55 -0800

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,7 @@
 #!/bin/usr/python3
 def update_dictionary(a_dictionary, key, value):
-    b_dictionary = {key: value}
-    a_dictionary.update(b_dictionary)
+    if key in a_dictionary[key]:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
     return a_dictionary
