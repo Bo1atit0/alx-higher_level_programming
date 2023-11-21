6d8ec1708b0d7ecd966f453fe5148d5bff0e4786
bo1atit0 <shorungbebolatito@gmail.com>
2023-11-21 14:33:35 -0800

    class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""this is a class square that defines a square"""
+
+
+class Square:
+    def __init__(self, size):
+        """contains a private instanve attribute size"""
+
+        self.size = size
