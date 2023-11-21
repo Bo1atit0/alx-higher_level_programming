c6671cde1251b0d6b33e59b6164b5da895793745
bo1atit0 <shorungbebolatito@gmail.com>
2023-11-21 15:19:56 -0800

    class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
     """defines a class"""
     def __init__(self, size=0):
         try:
-            if size not isinstance(size, int):
+            if not isinstance(size, int):
                 raise TypeError("size must be an integer")
             if size < 0:
                 raise ValueError("size must be >= 0")
