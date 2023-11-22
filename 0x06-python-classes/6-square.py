ebfbcb936b73b815f4baed9cde16a4d8f5e6abf2
bo1atit0 <shorungbebolatito@gmail.com>
2023-11-22 10:35:47 -0800

    document

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -99,6 +99,7 @@ class Square:
         """
         if self.__size == 0:
             print()
+            return
         else:
             for _ in range(self.__position[1]):
                 print()
