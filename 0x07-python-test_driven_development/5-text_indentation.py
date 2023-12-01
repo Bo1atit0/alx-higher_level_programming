4effd55a2edd635d8c5c2b7a9cf1e6fa03aad02a
bo1atit0 <shorungbebolatito@gmail.com>
2023-11-30 16:28:23 -0800

    text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Text Indentation module
+Prototype: def text_indentation(text):
+Raises:
+  exceptions if text is not a string.
+"""
+
+
+def text_indentation(text):
+    """
+    A text indentation Function that
+    prints 2 new lines after these chars:
+    ".", "?" and ":"
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    # variable for final string
+    result_str = ''
+
+    # flag for spaces at the beginning of newline
+    start_of_line = True
+
+    for char in text:
+
+        if char in ['.', '?', ':']:
+            result_str += char + '\n\n'
+            start_of_line = True
+        elif not start_of_line or char != " ":
+            result_str += char + ''
+            start_of_line = False
+    print(result_str)
