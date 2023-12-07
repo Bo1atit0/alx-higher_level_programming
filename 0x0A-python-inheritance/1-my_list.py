f3e8b4a86fa09ae6f9400605384a52f75d15735c
bo1atit0 <shorungbebolatito@gmail.com>
2023-12-07 00:17:58 -0800

    lists

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+a class MyList that inherits from list built_in -
+- data type(class)
+def print_sorted(self): that prints the list,
+but sorted
+"""
+
+
+class MyList(list):
+    """
+    a class MyList that inherits from list
+    """
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list) 
