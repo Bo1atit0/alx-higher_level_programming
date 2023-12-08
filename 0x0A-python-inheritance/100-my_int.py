ba2f9304c1a8195afb37a4bc732b252054cee5b9
bo1atit0 <shorungbebolatito@gmail.com>
2023-12-08 07:11:59 -0800

    reverse equality

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+class MyInt(int):
+    """
+    a class inheriting from the built-in int class
+    """
+    # def __init__(self, value):
+    # self.value = value
+
+    def __eq__(self, other):
+
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        return not super().__ne__(other)
