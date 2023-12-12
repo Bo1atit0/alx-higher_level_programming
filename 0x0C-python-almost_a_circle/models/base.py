35532b6a7b18e615c4c7c3a042ec157684198c1a
bo1atit0 <shorungbebolatito@gmail.com>
2023-12-12 21:26:25 +0100

    json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,6 +7,7 @@ class constructor: def __init__(self, id=None)
 if id is not None, id = public instance attribute id
 else, increment __nb_objects and assign value to id
 """
+import json
 
 
 class Base:
@@ -24,3 +25,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
