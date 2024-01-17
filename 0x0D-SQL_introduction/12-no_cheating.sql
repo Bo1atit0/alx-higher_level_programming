59a3f3a8369b2df1101bfe999bd108fb8fe97a54
bo1atit0 <shorungbebolatito@gmail.com>
2024-01-17 01:51:02 +0100

    list all records

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -2,4 +2,4 @@
 -- table second_table
 -- You are not allowed to use Bob’s id value, only the name field
 
-UPDATE second_table SET score = 10 WHERE 'name' = 'Bob'
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
