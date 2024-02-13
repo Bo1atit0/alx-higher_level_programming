37de2e11b2bdd86407e8fc0e248a4975fd22a191
bo1atit0 <shorungbebolatito@gmail.com>
2024-02-13 16:34:44 +0100

    size

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let i, j;
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+}
+for (i = 0; i < size; i++) {
+  let square = '';
+  for (j = 0; j < size; j++) {
+    square += 'x';
+  }
+  console.log(square);
+}
