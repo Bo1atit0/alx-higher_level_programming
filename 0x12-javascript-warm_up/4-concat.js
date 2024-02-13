49449820f079159257b86d84864a88d31a99087b
bo1atit0 <shorungbebolatito@gmail.com>
2024-02-13 11:51:28 +0100

    first is second

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+if (firstArg === undefined && secondArg === undefined) {
+  console.log('undefined is undefined');
+} else {
+  console.log(firstArg, 'is', secondArg);
+}
