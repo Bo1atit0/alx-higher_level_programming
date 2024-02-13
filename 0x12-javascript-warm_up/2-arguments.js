85901bbec71fb406678a8f04ced8751a9325a741
bo1atit0 <shorungbebolatito@gmail.com>
2024-02-13 01:39:19 +0100

    args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) {
+let args = process.argv.length
+
+if (args < 3) {
   console.log('No argument');
+} else if (args == 3) {
+  console.log('Argument found');
 } else {
   console.log('Argument found');
 }
