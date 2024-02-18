6f292fd7efb6e53016849dee633936fa7a9001b1
bo1atit0 <shorungbebolatito@gmail.com>
2024-02-18 20:37:01 +0100

    parameterized queries

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,44 @@
+#!/bin/usr/python3
+
+"""
+Write a script that takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa where name
+matches the argument.
+
+Your script should take 4 arguments:
+mysql username, mysql password, database name and state name searched
+(no argument validation needed)
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+You must use format to create the SQL query with the user input
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        host="localhost",
+        port=3306
+    )
+
+# Establish a connection to mysql server
+    cur = conn.cursor()
+# Execute a query using the execute() function
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    state_name = (argv[4], )
+    cur.execute(query, state_name)
+# Fetch all results
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    conn.close()
