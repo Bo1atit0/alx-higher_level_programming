99ce7db68664ec7479eb08867908abaa1b3adaf9
bo1atit0 <shorungbebolatito@gmail.com>
2024-02-18 21:30:59 +0100

    parameterized queries

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,50 +1,52 @@
 #!/usr/bin/python3
 
 """
-Write a script that takes in an argument and displays all
-values in the states table of hbtn_0e_0_usa where name
-matches the argument.
-
-Your script should take 4 arguments:
-mysql username, mysql password, database name and state name searched
-(no argument validation needed)
-You must use the module MySQLdb (import MySQLdb)
-Your script should connect to a MySQL server running on localhost at port 3306
-You must use format to create the SQL query with the user input
-Results must be sorted in ascending order by states.id
-Results must be displayed as they are in the example below
-Your code should not be executed when imported
+Script that displays all values in the states table of hbtn_0e_0_usa
+where name matches the provided argument.
+Parameters for script: mysql username, mysql password, database name
+and state name searched.
+Must use the `MySQLdb` module.
+Script should connect to a MySQL server runnimg on `localhost` at port `3306`
+Must use `format` to create the SQL query with the user input.
+Results must be in ascending order by `states.id`.
+Code should not be executed when imported.
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    # add check to see if number of arguments is correct
     if len(argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>"
-              .format(argv[0]))
+        print("Usage: {} <username> <password> <database> <state_name>".format(
+            argv[0]))
         exit()
 
-    conn = MySQLdb.connect(
+    # establishing a secure connection to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=argv[1],
         passwd=argv[2],
-        db=argv[3],
-        host="localhost",
-        port=3306
+        db=argv[3]
     )
 
-# Establish a connection to mysql server
-    cur = conn.cursor()
-# Execute a query using the execute() function
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        argv[4])
-    cur.execute(query)
-# Fetch all results
-    states = cur.fetchall()
+    # creating a cursor object to execute SQL queries
+    cursor = db.cursor()
 
+    # using format to create the SQL query with the user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+            argv[4])
+    cursor.execute(query)
+
+    # fetching all the results
+    states = cursor.fetchall()
+
+    # display/print them out
     for state in states:
         if state[1] == argv[4]:
             print(state)
 
-    cur.close()
-    conn.close()
+    # closing the cursor and database connection
+    cursor.close()
+    db.close()
