95bd7ba87ffbc16f6feaf42c9abe1c1ce4210e7c
bo1atit0 <shorungbebolatito@gmail.com>
2024-04-11 05:12:48 +0100

    rec

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,14 +3,11 @@
 class Rectangle {
   width;
   height;
-
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return {};
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
-
 module.exports = Rectangle;
