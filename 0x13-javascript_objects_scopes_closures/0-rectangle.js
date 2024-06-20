940e5e345d80b8da0769a3042c3c55b8b8cc5721
bo1atit0 <shorungbebolatito@gmail.com>
2024-06-20 20:30:17 +0100

    js

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // an empty class
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const sQuare = require('./5-square');
+
+class Square extends sQuare {
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      let i;
+      const row = c.repeat(this.width);
+
+      for (i = 0; i < this.width; i++) {
+        console.log(row);
+      }
+    }
+  }
+}
+module.exports = Square;
