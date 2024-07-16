bba9b5b80f1d001ef37211536d2f8dd5f894db77
bolatito <shorungbebolatito@gmail.com>
2024-07-16 16:51:32 +0100

    size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Write a Bash script that takes in a URL,
+# sends a request to that URL,
+# and displays the size of the body of the response
+
+if [ -z "$1" ];
+then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+
+URL=$1
+
+SIZE=$(curl -s -o /dev/null -w '%{size_download}' "$UR"L)
+
+echo "$SIZE"
